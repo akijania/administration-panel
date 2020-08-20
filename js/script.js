9dbdf49200ae74a0898028de4ce6c62b3aa738ed
Anna Kijania <kijania.anna@gmail.com>
2020-08-20 21:04:11 +0200

    Add modal quit

--- js/script.js
+++ js/script.js
@@ -21,10 +21,46 @@ for (let link of links) {
 
 
 function toggleMenu(visible) {
-    document.querySelector('.sidebar').classList.toggle('show', visible)
+    document.querySelector('.sidebar').classList.toggle('showMenu', visible)
 };
 
 document.querySelector('.hamburger').addEventListener('click', function (e) {
     e.preventDefault();
     toggleMenu()
 });
+
+function closeModal() {
+    document.getElementById('overlay').classList.remove('show')
+  }
+  document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault()
+      closeModal()
+    })
+  })
+  document.querySelector('#overlay').addEventListener('click', function(e) {
+    if(e.target === this) {
+      closeModal()
+    }
+  })
+  document.addEventListener('keyup', function(e) {
+    if(e.keyCode === 27) {
+      closeModal()
+    }
+  })
+  function openModal(modal) {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show')
+    })
+    document.querySelector('#overlay').classList.add('show')
+    document.querySelector(modal).classList.add('show')
+  }
+  const quitLinks = document.querySelectorAll('.quit');
+
+for (let quitLink of quitLinks) {
+    quitLink.addEventListener('click', function(event) {
+        event.preventDefault();
+          openModal('#modalQuit')
+        });
+}
+  
